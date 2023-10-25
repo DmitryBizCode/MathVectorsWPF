a5ad678d975b17e8ca5cf0b321a10111b5036ff1
Dmytro Beznosiuk <dmitrybeznosiukcode@gmail.com>
2023-10-25 21:07:18 +0300

    AddALLFunctional

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,29 @@ namespace Mathematical_vectors
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double XVA = double.Parse(this.XVA.Text);
+                double YVA = double.Parse(this.YVA.Text);
+                double ZVA = double.Parse(this.ZVA.Text);
+                double XVB = double.Parse(this.XVB.Text);
+                double YVB = double.Parse(this.YVB.Text);
+                double ZVB = double.Parse(this.ZVB.Text);
+                var VMF = new VectorMathFunc(XVA, YVA, ZVA, XVB, YVB, ZVB);
+                addition.Content = "("+VMF.Plus()[0] + ";" + VMF.Plus()[1] + ";" + VMF.Plus()[2] +")";
+                subtraction.Content = "(" + VMF.Minus()[0] + ";" + VMF.Minus()[1] + ";" + VMF.Minus()[2] + ")";
+                Scalar.Content = Math.Round(VMF.Scalar(),3);
+                LengthA.Content = Math.Round(VMF.Length()[0],3);
+                LengthB.Content = Math.Round(VMF.Length()[1],3);
+                LengthAB.Content = Math.Round(VMF.Length()[2],3);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
---
+++ VectorMathFunc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematical_vectors
+{
+    public class VectorMathFunc
+    {
+        private readonly double xa, xb, yb, zb, ya, za;
+        public VectorMathFunc(double XA, double YA, double ZA, double XB, double YB, double ZB) {
+            try
+            {
+                xa = XA;
+                ya = YA;
+                za = ZA;
+                zb = ZB;
+                xb = XB;
+                yb = YB;
+            }
+            catch (Exception)
+            {
+                throw new Exception("incorrect data");
+            }
+        }
+
+        public double[] Plus()
+        {
+            double[] result = new double[3];
+            result[0] = xa + xb;
+            result[1] = ya + yb;
+            result[2] = za + zb;
+            return result;
+        }
+
+        public double[] Minus()
+        {
+            double[] result = new double[3];
+            result[0] = xa - xb;
+            result[1] = ya - yb;
+            result[2] = za - zb;
+            return result;
+        }
+
+        public double Scalar() { 
+            return xa* xb + ya* yb + za* zb;
+        }
+
+        public double[] Length()
+        {
+            double[] result = new double[3];
+            result[0] = Math.Sqrt(Math.Pow(xa,2)+ Math.Pow(ya, 2) + Math.Pow(za, 2));
+            result[1] = Math.Sqrt(Math.Pow(xb, 2) + Math.Pow(yb, 2) + Math.Pow(zb, 2));
+            result[2] = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+            return result;
+        }
+    }
+}
